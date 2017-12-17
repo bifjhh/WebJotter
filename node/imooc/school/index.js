0e97dd36f992bf6f746cebe5c86209efc97edc4f
bifjhh <929160004@qq.com>
2017-12-17 18:59:19 +0800

    引入

--- node/imooc/school/index.js
+++ node/imooc/school/index.js
@@ -1,2 +1,9 @@
 var klass = require('./klass');
-klass.add('Scott',['白富美','高富帅']);
+exports.add = function (klasses) {
+    klasses.forEach(function (item, index) {
+        var _klass = item;
+        var teacherName = item.teacherName;
+        var students = item.students;
+        klass.add(teacherName, [students]);
+    });
+}
