7648ec3877a62a14828b166e8359a003c67b7d3c
bifjhh <929160004@qq.com>
2018-02-13 00:44:31 +0800

    node 环境执行js文件 输出

---
+++ node/itcast/01-hello-world/01-hello-world.js
@@ -0,0 +1,19 @@
+// var m= 10,n=100;
+// function add(x,y){
+//     return x+y
+// };
+// var result =add(m,n);
+// console.log('计算的结果是:'+result);
+
+// for(let i =0;i<10;i++){
+//     for(let j =0;j<=i;j++){
+//         process.stdout.write('*');
+//     }
+//     console.log();
+// }
+
+
+// process 模块在使用的时候无需通过 require()函数来加载该模块,可以直接使用
+// fs 模块,在使用的时候,必须通过require()函数来加载该模块,方可使用 var fs = require('fs');
+// 原因: process 模块是全局模块,而 fs 模块不是全局模块,全局模块可以直接使用,而非全局模块需要先通过 require('') 加载该模块
+
