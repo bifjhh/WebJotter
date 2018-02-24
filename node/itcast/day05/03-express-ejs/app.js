a8601250e101cb6e0272451dc2951b8ec8403ae4
bifjhh <929160004@qq.com>
2018-02-24 17:19:01 +0800

    创建一个自己的模板引擎,用来识别后缀是 html 的模板文件

--- node/itcast/day05/03-express-ejs/app.js
+++ node/itcast/day05/03-express-ejs/app.js
@@ -10,19 +10,31 @@ var path = require('path');
 // 2. 创建 app 对象
 var app = express();
 
-// 为 express 继承 ejs 模板引擎(配置 express 使用 ejs 模板引擎)
-// 配置完毕后, res.render() 函数就可以使用了
-// 配置模板引擎的代码要放到正式处理请求之前,否则可能不能用
-// 当使用 ejs 模板引擎的时候,模板文件的后缀必须是 .ejs 
-// 1. 安装 ejs 模块
-// 2. 配置 express 使用 ejs 模板引擎
-// - 告诉 express , 模板文件的存放路径
-// - 告诉 express , 要使用的模板引擎 
+// ----- 配置使用 ejs 模板引擎 -------
+// // 为 express 继承 ejs 模板引擎(配置 express 使用 ejs 模板引擎)
+// // 配置完毕后, res.render() 函数就可以使用了
+// // 配置模板引擎的代码要放到正式处理请求之前,否则可能不能用
+// // 当使用 ejs 模板引擎的时候,模板文件的后缀必须是 .ejs 
+// // 1. 安装 ejs 模块
+// // 2. 配置 express 使用 ejs 模板引擎
+// // - 告诉 express , 模板文件的存放路径
+// // - 告诉 express , 要使用的模板引擎 
 
-// // 设置所有模板文件的存放路径
+// // // 设置所有模板文件的存放路径
+// app.set('views', path.join(__dirname, 'htmls'));
+// // // 设置要使用 ejs 模板引擎
+// app.set('view engine', 'ejs');
+
+
+// ----- 配置使用 ejs 模板引擎 修改默认查找的模板文件后缀为 html -------
+// 1. 设置模板文件的存放路径 
 app.set('views', path.join(__dirname, 'htmls'));
-// // 设置要使用 ejs 模板引擎
-app.set('view engine', 'ejs');
+
+// 2. 创建一个自己的模板引擎,用来识别后缀是 html 的模板文件
+app.engine('html',require('ejs').renderFile)
+
+// 3. 使用刚才自己创建的这个模板引擎
+app.set('view engine', 'html');
 
 
 // 3. 注册路由
