529d1d3d7842dde6613b7a9388b9444957a92772
bifjhh <929160004@qq.com>
2018-02-26 20:39:56 +0800

    通过 request 直接下载文件

--- node/itcast/day06/03-request-demo/index.js
+++ node/itcast/day06/03-request-demo/index.js
@@ -6,12 +6,18 @@ var path = require('path');
 
 // 1. 直接调用 request 方法请求 url,并且通过回调函数获取返回值
 
-request("https://www.qiushibaike.com/", (err, res, body) => {
-  // console.log(err);
-  // console.log(res);
-  // console.log(body);
-  if(err)throw err;
-fs.writeFile(path.join(__dirname, 'body.html'), body, (err) => {
-    console.log('ok');
-  })
-})
+// request("https://www.qiushibaike.com/", (err, res, body) => {
+//   // console.log(err);
+//   // console.log(res);
+//   // console.log(body);
+//   if(err)throw err;
+// fs.writeFile(path.join(__dirname, 'body.html'), body, (err) => {
+//     console.log('ok');
+//   })
+// })
+
+// 2. 通过 request 直接下载文件
+// request('https://www.baidu.com/img/bd_logo1.png').pipe(fs.createWriteStream('./Logo.png'));
+
+
+request('https://www.qiushibaike.com/').pipe(fs.createWriteStream('./糗百.html'));
