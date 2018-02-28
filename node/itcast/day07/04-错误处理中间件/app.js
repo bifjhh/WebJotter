a3be1c492a609c63e97f014bd144437255baa77e
bifjhh <929160004@qq.com>
2018-02-28 11:30:06 +0800

    使用错误处理中间件 处理 err 错误

---
+++ node/itcast/day07/04-错误处理中间件/app.js
@@ -0,0 +1,53 @@
+// app.js  模块职责: 负责启动服务
+
+// 1. 加载 express 模块
+var express = require('express');
+// 加载 config.js 模块
+var config = require('./config.js');
+var reouter = require('./reouter.js');
+var path = require('path');
+// 加载 body-parser 模块
+var bodyParser = require('body-parser');
+
+// 2. 创建 app 对象
+var app = express();
+
+// ----- 配置使用 ejs 模板引擎 -------
+// // 为 express 继承 ejs 模板引擎(配置 express 使用 ejs 模板引擎)
+// // 配置完毕后, res.render() 函数就可以使用了
+// // 配置模板引擎的代码要放到正式处理请求之前,否则可能不能用
+// // 当使用 ejs 模板引擎的时候,模板文件的后缀必须是 .ejs 
+// // 1. 安装 ejs 模块
+// // 2. 配置 express 使用 ejs 模板引擎
+// // - 告诉 express , 模板文件的存放路径
+// // - 告诉 express , 要使用的模板引擎 
+
+// // // 设置所有模板文件的存放路径
+// app.set('views', path.join(__dirname, 'htmls'));
+// // // 设置要使用 ejs 模板引擎
+// app.set('view engine', 'ejs');
+
+
+// ----- 配置使用 ejs 模板引擎 修改默认查找的模板文件后缀为 html -------
+// 1. 设置模板文件的存放路径 
+app.set('views', path.join(__dirname, 'htmls'));
+
+// 2. 创建一个自己的模板引擎,用来识别后缀是 html 的模板文件
+app.engine('html',require('ejs').renderFile)
+
+// 3. 使用刚才自己创建的这个模板引擎
+app.set('view engine', 'html');
+
+// 配置 body-parser 插件(挂载 body-parser 中间件)
+// 一定要在正式处理请求之前设置 body-parser
+// 如果参数 extended:false 表示在 bodyparser 内部使用 node.js 内置的 querystring 模块把用户 post 提交的查询字符串转换为 json 对象
+// 如果参数 extended:true 表示在 bodyparser 内部使用 第三方的 qs 模块 把用户 post 提交的查询字符串转换为 json 对象
+app.use(bodyParser.urlencoded({extended:false}));
+
+// 3. 注册路由
+// app.use('/', reouter); == // app.use(reouter);
+app.use(reouter);
+
+// 4. 启动服务
+
+app.listen(config.port, () => console.log('http://localhost:' + config.port));
---
+++ node/itcast/day07/04-错误处理中间件/db.js
@@ -0,0 +1,63 @@
+// 封装对 数据 的增删改查操作
+
+const mongodb = require('mongodb');
+const path = require('path');
+const config = require('./config.js');
+
+// 因为在每个方法中都会用到链接数据库的代码,所以吧链接数据库的代码单独封装到一个函数内
+function connectDB(callback){
+  var MongoClient = mongodb.MongoClient;
+  MongoClient.connect(config.url,(err,db)=>{
+    // 这里只是 用来连接数据库,结果全部传递到 callback 回调函数内;不做判断
+    callback(err,db);
+  })
+};
+
+// 封装一个根据 字符串 id 返回 objectID对象的方法
+module.exports.ObjectID= (strId)=>{
+  return new mongodb.ObjectID(strId);
+};
+
+// 封装查询所有数据的方法
+
+module.exports.findAll =(collectionName,callback)=>{
+  // 1. 调用 connectDB 链接数据库
+  connectDB((err,db)=>{
+    if(err){callback(err); return;};
+    // 2. 查询 news 集合中的所有数据
+    db.collection(collectionName).find().toArray((err,docs)=>{
+      db.close();
+      // 将查询到的结果返回出去
+      callback(err,docs);
+    })
+  })
+};
+
+// 封装一个根据条件查询返回一条数据的方法
+module.exports.findOne=(collectionName,filter,callback)=>{
+  // 1. 链接数据库
+  connectDB((err,db)=>{
+    if(err)throw err;
+
+    // 2. 根据条件查询数据库
+    db.collection(collectionName).findOne(filter,(err,doc)=>{
+      db.close();
+      callback(err,doc);
+    })
+  })
+};
+
+// 封装一个插入一条数据的方法
+module.exports.insertOne=(collectionName,data,callback)=>{
+  connectDB((err,db)=>{
+    if(err)throw err;
+    
+    // 2. 获取集合对象,执行插入操作
+    db.collection(collectionName).insertOne(data,(err,result)=>{
+      db.close();
+      callback(err,result);
+    })
+  })
+};
+
+
