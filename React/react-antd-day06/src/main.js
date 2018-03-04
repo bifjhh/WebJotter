7e433b3958b3ae046d23213a967b8950b21e4685
bifjhh <929160004@qq.com>
2018-03-04 23:55:37 +0800

    antd 使用😍

--- React/react-antd-day06/src/main.js
+++ React/react-antd-day06/src/main.js
@@ -3,7 +3,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'antd/dist/antd.css';
+// 由于 直接引入 Ant Design 的全部包,体积过大米锁义,建议大家使用 按需导入 这样就能减少 bundle.js 的体积了
+// import 'antd/dist/antd.css';
 
 import App from './App.jsx'
 // 引入全局的 css 样式
