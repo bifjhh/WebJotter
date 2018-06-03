f6094de31d106ac1eef176529d1aaa8a1b7d4c11
bifjhh <929160004@qq.com>
2018-06-04 00:15:05 +0800

    img

--- vue/ytwkt/src/views/home.vue
+++ vue/ytwkt/src/views/home.vue
@@ -2,7 +2,13 @@
   <div>
     <Banner></Banner>
     <div class="nav">
-      <div class="info" v-for="item in 12" :key="item">
+      <div class="info"  v-for="item in 6" :key="item">
+      </div>
+      <div class="info" v-for="item in 3" :key="'2'+item">
+      </div>
+      <div class="info" v-for="item in 2" :key="'3'+item">
+      </div>
+      <div class="info">
       </div>
     </div>
   </div>
@@ -31,9 +37,43 @@ import Banner from "@/components/Banner";
   box-sizing: border-box;
   width: 2.22rem;
   height: 1.48rem;
-  background-color: #fef;
   margin-right: 0.2rem;
   margin-top: 0.2rem;
   border-radius:.15rem;
+  background-size: 100%,100%;
+  background-image: url('../images/11.png');
+}
+.info:nth-of-type(2){
+  background-image: url('../images/12.png');
+}
+.info:nth-of-type(3){
+  background-image: url('../images/13.png');
+}
+.info:nth-of-type(4){
+  background-image: url('../images/14.png');
+}
+.info:nth-of-type(5){
+  background-image: url('../images/15.png');
+}
+.info:nth-of-type(6){
+  background-image: url('../images/16.png');
+}
+.info:nth-of-type(7){
+  background-image: url('../images/21.png');
+}
+.info:nth-of-type(8){
+  background-image: url('../images/21.png');
+}
+.info:nth-of-type(9){
+  background-image: url('../images/21.png');
+}
+.info:nth-of-type(10){
+  background-image: url('../images/31.png');
+}
+.info:nth-of-type(11){
+  background-image: url('../images/32.png');
+}
+.info:nth-of-type(12){
+  background-image: url('../images/33.png');
 }
 </style>
