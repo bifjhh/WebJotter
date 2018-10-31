c0cccfac8ca035e417f3f1c62fe4fa8fc27c4244
sunkun <sk@360shop.cn>
2018-10-31 09:06:55 +0800

    demo

--- node/itcast/day02/01-demo/02-demo.js
+++ node/itcast/day02/01-demo/02-demo.js
@@ -5,6 +5,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   console.log('有人访问了。');
   res.end('over');
-}).listen(9999, function () {
-  console.log('http://localhost:9999');
+}).listen(7560, function () {
+  console.log('http://localhost:7560');
 });
